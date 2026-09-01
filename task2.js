08ebc0a89320d64d9c73cd1071440db9b92976b1
nirmalvp4-afk <nirmalvp4@gmail.com>
2026-09-01 15:57:32 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,207 @@
+// // Task 1 — var, let, const
+// var studentName = "Raju;
+// let studentAge = 22;
+// const collegeName = "Marymatha College";
+
+// console.log(studentName, studentAge, collegeName);
+
+// studentName = "Arun";
+// studentAge = 23;
+
+// // collegeName = "ABC"; // Error
+
+// var studentName = "Kumar";
+// // let studentAge = 25; // Error
+
+
+// // Task 2 — User Information
+// let userName = prompt("Enter name:");
+// let userAge = prompt("Enter age:");
+// let userCity = prompt("Enter city:");
+
+// console.log("Name:", userName);
+// console.log("Age:", userAge);
+// console.log("City:", userCity);
+
+
+// // Task 3 — Welcome Message
+// let name1 = prompt("Enter your name:");
+// alert("Welcome " + name1 + "!");
+
+
+// // Task 4 — Age Calculator
+// let birthYear = prompt("Enter birth year:");
+// let age = new Date().getFullYear() - birthYear;
+
+// console.log("Birth Year:", birthYear);
+// console.log("Age:", age);
+
+
+// // Task 5 — Identify Data Types
+// console.log(typeof "Hello");
+// console.log(typeof 100);
+// console.log(typeof 25.5);
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+// // Task 6 — Student Data
+// let student = {
+//   name: "Naveen",
+//   age: 22,
+//   city: "Trichy",
+//   qualification: "BSc",
+//   isStudent: true
+// };
+
+// console.log(student);
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.qualification);
+// console.log(student.isStudent);
+
+
+// // Task 7 — Fruit Array
+// let fruits = ["Apple", "Mango", "Orange", "Banana", "Grapes", "Papaya"];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[fruits.length - 1]);
+// console.log(fruits.length);
+
+
+// // Task 8 — Basic Calculator
+// let a = 20;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+
+// // Task 9 — Shopping Bill
+// let shirt = 999;
+// let pant = 1499;
+// let shoes = 1999;
+
+// console.log("Total:", shirt + pant + shoes);
+
+
+// // Task 10 — Marks Calculation
+// let tamil = 80;
+// let english = 75;
+// let maths = 90;
+
+// let total = tamil + english + maths;
+// let avg = total / 3;
+
+// console.log("Total:", total);
+// console.log("Average:", avg);
+
+
+// // Task 11 — Post Increment
+// let x1 = 10;
+// let y1 = x1++;
+
+// console.log(x1);
+// console.log(y1);
+
+
+// // Task 12 — Pre Increment
+// let x2 = 10;
+// let y2 = ++x2;
+
+// console.log(x2);
+// console.log(y2);
+
+
+// // Task 13 — Post Decrement
+// let x3 = 20;
+// let y3 = x3--;
+
+// console.log(x3);
+// console.log(y3);
+
+
+// // Task 14 — Pre Decrement
+// let x4 = 20;
+// let y4 = --x4;
+
+// console.log(x4);
+// console.log(y4);
+
+
+// // Task 15 — Final Values
+// let a5 = 5;
+// let b5 = a5++;
+// let c5 = ++a5;
+// let d5 = b5--;
+
+// console.log(a5);
+// console.log(b5);
+// console.log(c5);
+// console.log(d5);
+
+
+// // Task 16 — Assignment Operators
+// let num = 10;
+
+// num += 5;
+// console.log(num);
+
+// num -= 3;
+// console.log(num);
+
+// num *= 2;
+// console.log(num);
+
+// num /= 4;
+// console.log(num);
+
+// num %= 3;
+// console.log(num);
+
+// num **= 2;
+// console.log(num);
+
+
+// // Task 17 — Mini Student Profile
+// let stuName = "Naveen";
+// let stuAge = 22;
+// let stuCity = "Trichy";
+// let stuCollege = "Marymatha College";
+
+// let subjects = ["Maths", "Science", "English", "Tamil", "Computer"];
+
+// let profile = {
+//   name: stuName,
+//   age: stuAge,
+//   city: stuCity,
+//   subjects: subjects,
+//   isStudent: true
+// };
+
+// console.log(profile.name);
+// console.log(profile.age);
+// console.log(profile.city);
+// console.log(subjects[0]);
+// console.log(subjects[subjects.length - 1]);
+// console.log(subjects.length);
+// console.log(profile);
+
+
+// // Final Challenge — User Calculator
+// let num1 = Number(prompt("Enter first number:"));
+// let num2 = Number(prompt("Enter second number:"));
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
